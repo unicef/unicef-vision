b97b6e681a9a684c401d6eec7439cf4dea6adcd7
Domenico DiNicola <dom.dinicola@gmail.com>
2019-02-06 10:45:10 -0500

    remove requirements

--- setup.py
+++ setup.py
@@ -2,24 +2,19 @@
 # -*- coding: utf-8 -*-
 import ast
 import codecs
-import os.path
+import os
 import re
-import subprocess
 import sys
-from codecs import open
-from distutils import log
-from distutils.errors import DistutilsError
 
 from setuptools import find_packages, setup
 from setuptools.command.install import install
-from setuptools.command.sdist import sdist as BaseSDistCommand
+from setuptools.command.test import test as TestCommand
 
-ROOT = os.path.realpath(os.path.dirname(__file__))
-init = os.path.join(ROOT, 'src', 'unicef_vision', '__init__.py')
+HERE = os.path.abspath(os.path.dirname(__file__))
+init = os.path.join(HERE, "src", "unicef_vision", "__init__.py")
+
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 _name_re = re.compile(r'NAME\s+=\s+(.*)')
-
-sys.path.insert(0, os.path.join(ROOT, 'src'))
 
 with open(init, 'rb') as f:
     content = f.read().decode('utf-8')
@@ -28,52 +23,10 @@ with open(init, 'rb') as f:
 
 
 def read(*files):
-    content = []
+    content = ''
     for f in files:
-        content.extend(codecs.open(os.path.join(ROOT, 'src', 'requirements', f), 'r').readlines())
-    return "\n".join(filter(lambda l:not l.startswith('-'), content))
-
-
-def check(cmd, filename):
-    out = subprocess.run(cmd, stdout=subprocess.PIPE)
-    f = os.path.join('src', 'requirements', filename)
-    reqs = codecs.open(os.path.join(ROOT, f), 'r').readlines()
-    existing = {re.split("(==|>=|<=>|<|)", name[:-1])[0] for name in reqs}
-    declared = {re.split("(==|>=|<=>|<|)", name)[0] for name in out.stdout.decode('utf8').split("\n") if name and not name.startswith('-')}
-    # existing = {name[:-1] for name in reqs if name and not name.startswith('-')}
-    # declared = {name for name in out.stdout.decode('utf8').split("\n") if name and not name.startswith('-')}
-    if existing - declared:
-        error = f"Unknown libraries: {existing-declared}"
-    else:
-        error = f"Missing libraries: {declared-existing}"
-
-        if existing != declared:
-            msg = f"""Requirements file not updated.
-
-    {error}
-
-    To fix run:
-
-    {' '.join(cmd)} > {f}
-
-    """
-            raise DistutilsError(msg)
-
-#     if existing != declared:
-#         msg = """Requirements file not updated.
-# Run 'make requiremets'
-# """.format(' '.join(cmd), f)
-#         raise DistutilsError(msg)
-
-class SDistCommand(BaseSDistCommand):
-
-    def run(self):
-        checks = {'install.pip': ['pipenv', 'lock', '--requirements'],
-                  'testing.pip': ['pipenv', 'lock', '-d', '--requirements']}
-
-        for filename, cmd in checks.items():
-            check (cmd, filename)
-        super().run()
+        content += codecs.open(os.path.join(HERE, f), 'r').read()
+    return content
 
 
 class VerifyTagVersion(install):
@@ -89,30 +42,43 @@ class VerifyTagVersion(install):
             sys.exit(info)
 
 
-setup(name=NAME,
-      version=VERSION,
-      url='https://github.com/unicef/unicef-vision',
-      author='UNICEF',
-      author_email='dev@unicef.org',
-      license="Apache 2 License",
-      description='Django package that handles exporting of data',
-      long_description=codecs.open('README.rst').read(),
-      package_dir={'': 'src'},
-      packages=find_packages(where='src'),
-      include_package_data=True,
-      install_requires=read('install.pip'),
-      extras_require={
-          'test': read('install.pip', 'testing.pip'),
-      },
-      platforms=['any'],
-      classifiers=[
-          'Environment :: Web Environment',
-          'Programming Language :: Python :: 3.6',
-          'Framework :: Django',
-          'Intended Audience :: Developers'],
-      scripts=[],
-      cmdclass={
-          # 'sdist': SDistCommand,
-          "verify": VerifyTagVersion,
-      }
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ''
+
+    def run_tests(self):
+        import shlex
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    url='https://github.com/unicef/unicef-vision',
+    author='UNICEF',
+    author_email='rapidpro@unicef.org',
+    description='',
+    long_description=read('README.rst'),
+    platforms=['any'],
+    license='Apache 2 License',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    cmdclass={"verify": VerifyTagVersion,
+              'test': PyTest,
+              }
 )
