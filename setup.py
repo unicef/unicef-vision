c51a9ca993d72cdf357652e5ac7dfa137ceecb66
Domenico <dom.dinicola@gmail.com>
2021-04-08 09:02:17 +0200

    added support to dj32

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ setup(
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
         'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
     ],
     install_requires=[
         'Django',
